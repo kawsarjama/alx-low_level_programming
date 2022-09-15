644b45f12f6a382ad0ec6453b7036591e686e4b9
kauthar jama <you@example.com>
2022-09-14 19:33:24 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,42 @@
-#include "main.h"
-/**
-* print_alphabet_x10 - Make alphabet x10 times
-* Return: void
-*/
-void print_alphabet_x10(void)
-{
-char c;
-int i = 0;
-while (i <= 9)
-{
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-i++;
-}
+#include "main.h"                                                                                                                       
+
+/**                                                                                                                                     
+
+ * print_alphabet_x10 - Make alphabet x10 times                                                                                         
+
+ *                                                                                                                                      
+
+ * Return: 0                                                                                                                            
+
+ */                                                                                                                                     
+
+void print_alphabet_x10(void)                                                                                                           
+
+{                                                                                                                                       
+
+        char c, i;                                                                                                                      
+
+        for (i = 0; i <= 9; i++)                                                                                                        
+
+        {                                                                                                                               
+
+                for (c ='a'; c <= z; c++)                                                                                               
+
+                {                                                                                                                       
+
+                        _putchar(c);                                                                                                    
+
+                }                                                                                                                       
+
+                _putchar('\n');                                                                                                         
+
+        }                                                                                                                               
+
+}                                                                                                                                       
+
+
+
+
+
+
 
